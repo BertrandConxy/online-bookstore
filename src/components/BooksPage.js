d682ed11caf5cc9474ef65461c5ac829481aaf1b
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-02-28 12:56:41 +0200

    Change the path of different files and Add routes

---
+++ src/components/BooksPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BooksPage = () => (
+  <div>BooksPage</div>
+);
+
+export default BooksPage;
---
+++ src/components/CategoriesPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CategoriesPage = () => (
+  <div>CategoriesPage</div>
+);
+
+export default CategoriesPage;
