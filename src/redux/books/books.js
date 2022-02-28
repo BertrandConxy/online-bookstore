1636c579044217e02c27ef8e7f8dab04f065e880
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-02-28 16:29:02 +0200

    Write codes for the books reducer

---
+++ src/redux/books/books.js
@@ -0,0 +1,30 @@
+const ADD_BOOK = 'online-bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'online-bookstore/books/REMOVE_BOOK';
+
+const initialState = [];
+
+export const addBook = (payload) => ({
+  type: ADD_BOOK,
+  payload,
+});
+
+export const removeBook = (id) => ({
+  type: REMOVE_BOOK,
+  id,
+});
+
+const reducer = (state = initialState, { type, payload, id }) => {
+  switch (type) {
+    case ADD_BOOK:
+      return [
+        ...state, payload,
+      ];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== id);
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
