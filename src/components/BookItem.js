06dffe0c43cb21740b0ce4fadacd580135addd6a
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-03-02 17:11:38 +0200

    Add styling to the book item component

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { loadBooks, removeBook } from '../redux/books/books';
+import React, { useEffect } from "react";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { useSelector, useDispatch } from "react-redux";
+import { loadBooks, removeBook } from "../redux/books/books";
 
 const BookItem = () => {
   const dispatch = useDispatch();
@@ -21,22 +22,35 @@ const BookItem = () => {
           <div key={book.item_id} className="book-item">
             <div className="book-details">
               <span className="book-category">{book.category}</span>
-              <h2>{book.title}</h2>
-              <span>Bertrand Ish</span>
+              <h2 className="book-title">{book.title}</h2>
+              <span className="book-author">Bertrand Ish</span>
               <div className="links-container">
                 <ul>
                   <li>
-                    <button id={book.item_id} type="button">
+                    <button
+                      id={book.item_id}
+                      className="btn-1 line"
+                      type="button"
+                    >
                       Comments
                     </button>
                   </li>
                   <li>
-                    <button id={book.item_id} type="button">
+                    <button
+                      id={book.item_id}
+                      className="btn-1 line"
+                      type="button"
+                    >
                       Edit
                     </button>
                   </li>
                   <li>
-                    <button id={book.item_id} type="button" onClick={removeHandler}>
+                    <button
+                      id={book.item_id}
+                      className="btn-1"
+                      type="button"
+                      onClick={removeHandler}
+                    >
                       Remove
                     </button>
                   </li>
@@ -44,20 +58,33 @@ const BookItem = () => {
               </div>
             </div>
             <div className="progress">
-              <span>5%</span>
-              <span>completed</span>
+              <CircularProgress
+                className="progressBar"
+                variant="determinate"
+                color="primary"
+                sx={{
+                  width: 500,
+                }}
+                value={50}
+              />
+              <div className="numerics">
+                <span className="number">5%</span>
+                <span className="completed">completed</span>
+              </div>
             </div>
             <div className="view-progress">
-              <span>Current chapter</span>
-              <h3>Chapter 1</h3>
-              <button id={book.item_id} type="button">Update progress</button>
+              <span className="chapter-title">Current chapter</span>
+              <h3 className="chapter-number">Chapter 1</h3>
+              <button id={book.item_id} type="button" className="btn-2">
+                Update progress
+              </button>
             </div>
           </div>
         ))}
       </>
     );
   }
-  return (<h2>No books added yet</h2>);
+  return <h2>No books added yet</h2>;
 };
 
 export default BookItem;
